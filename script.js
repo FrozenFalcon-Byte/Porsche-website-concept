e614c25429280724097f0d2643713b021c5b9d8e
Ajinkya Chavan <72398926+FrozenFalcon-Byte@users.noreply.github.com>
2025-01-20 19:43:37 +0530

    added responsiveness

--- script.js
+++ script.js
@@ -5,62 +5,80 @@ Shery.hoverWithMediaCircle(".hover", {videos: []}); //yes i have left this inten
 gsap.set(".bigimage", {
   y: "-70%",
   width: "90%",
-})
+});
+let mm = gsap.matchMedia();
 
-var tl1 = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#homemain",
-    // scroller: "#main",
-    pin: true,
-    // markers: true,
-    start: "-10% top",
-    end: "120% center",
-    scrub: 1,
-  },
-})
+mm.add("(min-width: 768px)", () => {
+  var tl1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#homemain",
+      pin: true,
+      start: "-15% top",
+      end: "120% center",
+      scrub: 1,
+    },
+  });
 
-tl1.to(".bigimage", {
-  y: "-100%",
-  ease: "power1.out",
-}).to(".bigimage", {
-  width: "100%",
-  ease: "power1.out",
-})
+  tl1.to(".bigimage", {
+    y: "-100%",
+    ease: "power1.out",
+  }).to(".bigimage", {
+    width: "100%",
+    ease: "power1.out",
+  });
 
-
-
-gsap.to(".elem", {
-  scrollTrigger: {
-    trigger: "#models",
-    pin: true,
-    start: "top top",
-    end: "+=5000",
-    // markers: true,
-    endTrigger: ".last",
-    scrub: 1,
-  },
-  y: "-400%",
-  ease: "power1.out",
+  return () => {
+    tl1.kill(); // Cleanup when conditions no longer match
+  };
 });
 
 
-let sections = document.querySelectorAll(".elem")
+mm.add("(min-width: 768px)", () => {
+  gsap.to(".elem", {
+    scrollTrigger: {
+      trigger: "#models",
+      pin: true,
+      start: "top top",
+      end: "+=5000",
+      // markers: true,
+      endTrigger: ".last",
+      scrub: 1,
+    },
+    y: "-400%",
+    ease: "power1.out",
+  });
 
-Shery.imageEffect(".images", {
-  style: 4,
-  // debug: true,
-  // config: {},
-  slideStyle: (setScroll) => {
-    sections.forEach(function (section, index) {
-      ScrollTrigger.create({
-        trigger: section,
-        start: "top top",
-        scrub: 1,
-        onUpdate: function (prog) {
-          setScroll(prog.progress + index);
-        },
+  return () => {
+    ScrollTrigger.getAll().forEach(st => st.kill()); // Cleanup when screen size changes
+  };
+});
+
+
+let sections = document.querySelectorAll(".elem");
+
+// Use matchMedia to check for screen width
+// let mm = gsap.matchMedia();
+
+mm.add("(min-width: 768px)", () => {
+  Shery.imageEffect(".images", {
+    style: 4,
+    slideStyle: (setScroll) => {
+      sections.forEach(function (section, index) {
+        ScrollTrigger.create({
+          trigger: section,
+          start: "top top",
+          scrub: 1,
+          onUpdate: function (prog) {
+            setScroll(prog.progress + index);
+          },
+        });
       });
-    });
-  },
+    },
+  });
+
+  return () => {
+    ScrollTrigger.getAll().forEach((st) => st.kill()); // Cleanup on screen size change
+  };
 });
+
 
